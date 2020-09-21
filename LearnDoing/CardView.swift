30c4cb875feb6dc69ef3e86a55e4b0204303f64e
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-21 22:30:39 +0300

    Update CardView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- LearnDoing/CardView.swift
+++ LearnDoing/CardView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var gradient: [Color] = [Color("Color01"), Color("Color02")]
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Image("developer-no1")
+            
+            VStack {
+                
+                Text("SwiftUI")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("Better apps. Less code.")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.white)
+                    .italic()
+            }
+            .offset(y: -218)
+        }
+        .frame(width: 335, height: 545)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: gradient),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+        )
+        .cornerRadius(16)
+        .shadow(radius: 8)
     }
 }
+
+// MARK: - PREVIEW
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
