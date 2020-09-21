d2d27417e5cbc106638ede53f86c3edd4e36956f
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-21 22:15:19 +0300

    Create CardView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ LearnDoing/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  LearnDoing
+//
+//  Created by Abdelrahman Mohamed on 21.09.2020.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
