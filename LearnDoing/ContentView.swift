7b5e9cc8f3db0679d98a305adc204e09cd3fb1c5
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-21 21:16:25 +0300

    Initial Commit

---
+++ LearnDoing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnDoing
+//
+//  Created by Abdelrahman Mohamed on 21.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnDoing/LearnDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnDoingApp.swift
+//  LearnDoing
+//
+//  Created by Abdelrahman Mohamed on 21.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct LearnDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
