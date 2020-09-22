0371a7f4eaef5073375889859161e2a842dee97c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 01:01:36 +0300

    Update PlaySound.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- LearnDoing/PlaySound.swift
+++ LearnDoing/PlaySound.swift
@@ -10,14 +10,14 @@ import AVFoundation
 
 // MARK: - AUDIO PLAYER
 
-var audioPlayer: AVAudioPlayer = AVAudioPlayer()
+var audioPlayer: AVAudioPlayer?
 
 func playSound(sound: String, type: String) {
     
     if let path = Bundle.main.path(forResource: sound, ofType: type) {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-            audioPlayer.play()
+            audioPlayer?.play()
         } catch {
             print("Could not find and play the sound file.")
         }
