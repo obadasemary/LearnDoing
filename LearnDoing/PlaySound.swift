9351f17cd0beebc82f835ab89edcd6fdfe7997b4
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 00:06:15 +0300

    Create PlaySound.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ LearnDoing/PlaySound.swift
@@ -0,0 +1,25 @@
+//
+//  PlaySound.swift
+//  LearnDoing
+//
+//  Created by Abdelrahman Mohamed on 22.09.2020.
+//
+
+import Foundation
+import AVFoundation
+
+// MARK: - AUDIO PLAYER
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not find and play the sound file.")
+        }
+    }
+}
